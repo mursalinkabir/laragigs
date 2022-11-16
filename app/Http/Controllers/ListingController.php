e7c5833abf9036be2e10201834bc742251349d89
Mursalin Kabir <mail2mursalin@gmail.com>
2022-11-17 01:18:56 +0900

    scopefiler and search function

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -9,8 +9,9 @@ class ListingController extends Controller
 {
     //show all listings
     public function index(){
+       // dd(request('tag'));
         return view('listings.index', [
-            'listings' => Listing::all()
+            'listings' => Listing::latest()->filter(request(['tag','search']))->get()
         ]);
     }
     //shown single listing
--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Listing extends Model
 {
     use HasFactory;
+    // this will apture the tag from the controller filter(request(['tag']))
+    public function scopeFilter($query, array $filters){
+        if($filters['tag'] ?? false){
+            // sql like query
+            $query->where('tags', 'like', '%'.$filters['tag'].'%');
+        }
+        //searching filter 
+        if($filters['search'] ?? false){
+            // sql like query
+            $query->where('title', 'like', '%'.$filters['search'].'%')
+            ->orWhere('description', 'like', '%'.$filters['search'].'%')
+            ->orWhere('tags', 'like', '%'.$filters['search'].'%');
+        }
+    }
 }
