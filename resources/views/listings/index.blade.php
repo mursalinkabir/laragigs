83f802b409fa223883050997f200e78b3b16f099
Mursalin Kabir <mail2mursalin@gmail.com>
2022-11-28 01:46:09 +0900

    Pagination addition

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -15,4 +15,8 @@ class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"
 <x-listing-card :listing="$listing" />
 @endforeach
 </div>
+<div class="mt-6 p-4">
+  {{-- pagination  --}}
+  {{$listings->links()}}
+</div>
 </x-layout>
